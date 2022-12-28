28bce11485773f5ec8bbf48745ac95c9797a8ba9
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2022-12-28 13:43:14 +0300

    v 0.68.1

--- Assets/Scripts/BarSystem.cs
+++ Assets/Scripts/BarSystem.cs
@@ -27,7 +27,7 @@ public class BarSystem : MonoSingleton<BarSystem>
         isFinish = true;
         while (isFinish)
         {
-            amount += Time.deltaTime / 3;
+            amount += Time.deltaTime / 4;
             if (_goRight)
             {
                 _bar.transform.position = Vector2.Lerp(startPos.transform.position, finishPos.transform.position, amount);
